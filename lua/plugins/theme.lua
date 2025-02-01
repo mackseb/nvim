62792b92a2faa92809ed0a0367ec1d098221386b
SEBASTIAN MACK <mack.seb@gmail.com>
2025-02-01 11:17:34 +0100

    Update theme.lua

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,19 +1,20 @@
 return {
-  {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      {
-        set_dark_mode = function()
-          vim.api.nvim_set_option_value("background", "dark", {})
-        end,
-        set_light_mode = function()
-          vim.api.nvim_set_option_value("background", "light", {})
-        end,
-        update_interval = 3000,
-        fallback = "dark",
-      },
-    },
-  },
+  -- disabling auto change but maybe reactivate again if required
+  -- {
+  --   "f-person/auto-dark-mode.nvim",
+  --   opts = {
+  --     {
+  --       set_dark_mode = function()
+  --         vim.api.nvim_set_option_value("background", "dark", {})
+  --       end,
+  --       set_light_mode = function()
+  --         vim.api.nvim_set_option_value("background", "light", {})
+  --       end,
+  --       update_interval = 3000,
+  --       fallback = "dark",
+  --     },
+  --   },
+  -- },
   {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
